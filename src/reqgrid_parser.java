cc9caaf28530c95604ba6c9cb9b0a82c53d954bb
kangheel <jadenlee77@gmail.com>
2023-08-04 20:55:38 -0700

    edited parser.java and added parser for reqgrid
    reqgrid is a collection college information in a of Common App spreadsheet

--- src/reqgrid_parser.java
+++ src/reqgrid_parser.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class parser {
+public class reqgrid_parser {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Name of the file you want to parse:");
@@ -26,13 +26,16 @@ public class parser {
     private static void exportFile(String file_name) throws FileNotFoundException {
         Scanner fileInput = new Scanner(new File("./data/csv/"+file_name+".csv"),"UTF-8");
         PrintWriter exportFile = new PrintWriter("./export/"+file_name+"_trimmed.csv");
+        exportFile.println("?,,Deadlines,,,,,,App Fees,,Common App,,Supplements?,,Minimum Standard Test Policy?,,,Recommendations?,,,,\nCommon App Member,School Type,ED,EDII,EA,EAII,REA,RD/ Rolling,US,INTL,Personal Essay Req'd,C&G,Portfolio?,Writing,Test Policy,SAT/ACT Tests Used,INTL,TE,OE,MR,CR,Saves Forms");
         while (fileInput.hasNextLine()) {
             // System.out.println(fileInput.nextLine());
             String curLine = fileInput.nextLine();
-            while (curLine.contains(",,")) {
-                curLine = curLine.replace(",,",",");
+            // while (curLine.contains(",,")) {
+            //     curLine = curLine.replace(",,",",");
+            // }
+            if (!curLine.contains("Deadlines") && !curLine.contains("Common App Member")) {
+                exportFile.println(curLine);
             }
-            exportFile.println(curLine);
         }
         exportFile.close();
         if (file_name.contains("_")) {
